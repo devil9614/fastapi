89a2867bc0683d195cadef3eeaaa86a8a868a92d
devil9614 <sujanmukherjee9614@gmail.com>
2023-02-12 02:34:00 +0530

    get dynamic data

--- test.py
+++ test.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "Sujan",
+        "age": 23,
+        "location": "Kolkata"
+    }
+}
+
 
 @app.get("/")
 def index():
     return {"Hello": "World"}
+
+
+@app.get("/user/{id}")
+def get_user(id: int):
+    return students[id]
