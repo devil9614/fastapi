f5e35f10a6a0f3ac4ff7915f03a17bb0a5608b71
devil9614 <sujanmukherjee9614@gmail.com>
2023-02-12 03:14:45 +0530

    added post methord

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 from typing import Optional
 from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
-students = {
+user = {
     1: {
         "name": "Sujan",
         "age": 23,
         "location": "Kolkata"
     }
 }
+
+
+class User(BaseModel):
+    name: str
+    age: int
+    location: str
 
 
 @app.get("/")
@@ -19,12 +26,21 @@ def index():
 
 # @app.get("/user/{user_id}")
 # def get_user(user_id: int = Path(None, description="Needs the id of the user")):
-#     return students[user_id]
+#     return user[user_id]
 
 
 @app.get("/user/{user_id}")
 def get_admin(user_id: int, admin: Optional[str] = None):
     if admin == "True":
-        return students[user_id]
+        return user[user_id]
     else:
         return {"error_msg": "You need to be an admin"}
+
+
+@app.post("/create_user/{user_id}")
+def create_user(user_id: int, user_details: User):
+    if user_id in user:
+        return {"error_msg": "User already exists"}
+    else:
+        user[user_id] = user_details
+        return user[user_id]
